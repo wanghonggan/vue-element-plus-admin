cafa05c98c07d76eb1d1fe12d76db6e158410618
陈凯龙 <502431556@qq.com>
2022-04-20 10:14:22 +0800

    wip: Crud component development

--- src/components/Crud/src/Crud.vue
+++ src/components/Crud/src/Crud.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { PropType, watch } from 'vue'
+import { PropType, watch, ref } from 'vue'
 import { useDesign } from '@/hooks/web/useDesign'
-// import { Search } from '@/components/Search'
+import { Search } from '@/components/Search'
 import { filterCrudSchema } from './helper'
+import { ContentWrap } from '@/components/ContentWrap'
 // import { Table } from '@/components/Table'
 // import { propTypes } from '@/utils/propTypes'
 
 const { getPrefixCls } = useDesign()
+
 const prefixCls = getPrefixCls('crud')
 
 const props = defineProps({
@@ -14,14 +16,22 @@ const props = defineProps({
   schema: {
     type: Array as PropType<CrudSchema[]>,
     default: () => []
+  },
+  // search组件的props
+  searchProps: {
+    type: Object as PropType<Recordable>,
+    default: () => ({})
   }
 })
+
+const searchSchema = ref<FormSchema[]>([])
 
 // 监听 props.schema 过滤出所有组件的主体结构数据
 watch(
   () => props.schema,
   (crudSchema: CrudSchema[]) => {
-    console.log(filterCrudSchema(crudSchema))
+    const schemas = filterCrudSchema(crudSchema)
+    searchSchema.value = schemas.searchSchema
   },
   {
     immediate: true,
@@ -31,5 +41,11 @@ watch(
 </script>
 
 <template>
-  <div :class="`${prefixCls}-crud`">Crud</div>
+  <div :class="`${prefixCls}-crud`">
+    <ContentWrap>
+      <slot name="search">
+        <Search v-bind="searchProps" :schema="searchSchema" />
+      </slot>
+    </ContentWrap>
+  </div>
 </template>
--- src/components/Crud/src/helper.ts
+++ src/components/Crud/src/helper.ts
@@ -1,12 +1,12 @@
 import { eachTree } from '@/utils/tree'
 
-interface AllSchema {
+interface AllSchemas {
   searchSchema: FormSchema[]
 }
 
 // 过滤所有结构
-export const filterCrudSchema = (crudSchema: CrudSchema[]): AllSchema => {
-  const searchSchema = filterSearchSchema(crudSchema)
+export const filterCrudSchema = (crudSchema: CrudSchema[]): AllSchemas => {
+  const searchSchema = filterSearchSchema(crudSchema) || []
   return {
     searchSchema
   }
@@ -16,16 +16,20 @@ export const filterCrudSchema = (crudSchema: CrudSchema[]): AllSchema => {
 export const filterSearchSchema = (crudSchema: CrudSchema[]): FormSchema[] => {
   const searchSchema: FormSchema[] = []
   eachTree(crudSchema, (schemaItem: CrudSchema) => {
+    // 判断是否显示
     if (schemaItem?.search?.show) {
       const searchSchemaItem = {
+        // 默认为 input
+        component: schemaItem.search.component || 'Input',
         ...schemaItem.search,
         field: schemaItem.field,
         label: schemaItem.label
       }
 
+      // 删除不必要的字段
       delete searchSchemaItem.show
 
-      searchSchema.push(searchSchemaItem as FormSchema)
+      searchSchema.push(searchSchemaItem)
     }
   })
   return searchSchema
